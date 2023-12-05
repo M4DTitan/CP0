f9daad24f07374046bfd4bd788931daac9b667a4
Helderio Wafunga <99135814+M4DTitan@users.noreply.github.com>
2023-12-05 23:21:46 +0100

    restart

--- cp0flutter/lib/main.dart
+++ cp0flutter/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:cp0flutter/frontend/projects/feed.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:cp0flutter/routes/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
