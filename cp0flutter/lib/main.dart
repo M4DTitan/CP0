a4fa097f93705af139a3389414c2904ea74ae547
Helderio Wafunga <99135814+M4DTitan@users.noreply.github.com>
2023-12-05 11:11:57 +0100

    faz miau

--- cp0flutter/lib/main.dart
+++ cp0flutter/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'frontend/projects/feed.dart';
-import 'routes/routes.dart';
+import 'helper/binding.dart';
+import 'view/auth/login_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,13 +13,14 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CP-0',
       theme: ThemeData(
+        textTheme: const TextTheme(),
         primaryColor: const Color(0xFF120F42), // Primary color
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: Colors.deepPurple, // Secondary color
@@ -27,14 +28,12 @@ class MyApp extends StatelessWidget {
         hintColor: const Color(0x0ff1d19b), // Accent color
         scaffoldBackgroundColor: const Color(0xFF3C34E0), // Background color
         appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF22294), // App bar background color
+          backgroundColor: Color(0x0ff22294), // App bar background color
         ),
         useMaterial3: true,
       ),
-      initialRoute: Routes.initial,
-      onGenerateRoute: Routes.generateRoute,
       debugShowCheckedModeBanner: false,
-      home: const AllProjectsPage(),
+      home: LoginView(),
     );
   }
 }
